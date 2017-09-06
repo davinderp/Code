177e60d5c8a54850b2fe87b905bcfd0134325f4e
Prince Kumar <princek@smartdatainc.net>
2017-09-06 18:11:44 +0530

    Added New API for Get User From Token

--- HC.Patient/HC.Web/Controllers/PatientCommonController.cs
+++ HC.Patient/HC.Web/Controllers/PatientCommonController.cs
@@ -1,7 +1,14 @@
 using Audit.WebApi;
+using HC.Common;
 using HC.Patient.Entity;
 using HC.Patient.Service.PatientCommon.Interfaces;
+using HC.Patient.Service.Token.Interfaces;
+using HC.Patient.Web.Options;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace HC.Patient.Web.Controllers
 {
@@ -9,12 +16,25 @@ namespace HC.Patient.Web.Controllers
     public class PatientCommonController : Controller
     {
         private readonly IPatientCommonService _patientCommonService;
+        private readonly JwtIssuerOptions _jwtOptions;
+        private readonly ITokenService _tokenService;
+        private readonly JsonSerializerSettings _serializerSettings;
 
         #region Construtor of the class
-        public PatientCommonController(IPatientCommonService patientCommonService)
+        public PatientCommonController(IPatientCommonService patientCommonService, IOptions<JwtIssuerOptions> jwtOptions, ITokenService tokenService)
         {
             try
             {
+                _jwtOptions = jwtOptions.Value;
+                ThrowIfInvalidOptions(_jwtOptions);
+
+                _tokenService = tokenService;
+
+                _serializerSettings = new JsonSerializerSettings
+                {
+                    Formatting = Formatting.Indented
+                };
+
                 this._patientCommonService = patientCommonService;
             }
             catch
@@ -54,6 +74,72 @@ namespace HC.Patient.Web.Controllers
                     Message = "No Data",
                     StatusCode = 404
                 });
+            }
+        }
+
+        [HttpPost]
+        [Route("GetUserByToken")]
+        public JsonResult GetUserByToken(string Token)
+        {
+            CommonMethods commonMethods = new CommonMethods();
+            var encryptData = commonMethods.GetDataFromToken(Token);
+            int userid=0;
+            if (encryptData != null && encryptData.Claims!=null)
+            {
+                if (encryptData.Claims.Count > 0)
+                {
+                    userid = Convert.ToInt32(encryptData.Claims[0].Value);
+                    Response.StatusCode = 200;//(Not Found)
+                    var response = new
+                    {
+                        access_token = Token,
+                        expires_in = (int)_jwtOptions.ValidFor.TotalSeconds,
+                        data = _tokenService.GetDoctorByUserID(userid)
+                        
+                    };
+                    var json = JsonConvert.SerializeObject(response, _serializerSettings);
+                    return new JsonResult(json);
+                }
+                else
+                {
+                    Response.StatusCode = 401;//(Unauthorized)
+                    return Json(new
+                    {
+                        data = new object(),
+                        Message = "Invalid token",
+                        StatusCode = 401
+                    });
+                }
+                
+            }
+            else
+            {
+                Response.StatusCode = 401;//(Unauthorized)
+                return Json(new
+                {
+                    data = new object(),
+                    Message = "Invalid token",
+                    StatusCode = 401
+                });
+            }
+        }
+        private static void ThrowIfInvalidOptions(JwtIssuerOptions options)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            if (options.ValidFor <= TimeSpan.Zero)
+            {
+                throw new ArgumentException("Must be a non-zero TimeSpan.", nameof(JwtIssuerOptions.ValidFor));
+            }
+
+            if (options.SigningCredentials == null)
+            {
+                throw new ArgumentNullException(nameof(JwtIssuerOptions.SigningCredentials));
+            }
+
+            if (options.JtiGenerator == null)
+            {
+                throw new ArgumentNullException(nameof(JwtIssuerOptions.JtiGenerator));
             }
         }
         #endregion
